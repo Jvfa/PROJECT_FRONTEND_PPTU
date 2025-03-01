76d4a05f9de8c406c5dc403c51aba90c266907f7
Jvfa <joao.victor1feneriche@gmail.com>
2025-02-28 21:25:46 -0300

    adição da splash

--- App.js
+++ App.js
@@ -2,10 +2,26 @@ import React from "react";
 import { NavigationContainer } from "@react-navigation/native";
 import { createStackNavigator } from "@react-navigation/stack";
 import HomeScreen from "./app/screens/HomeScreen";
+import { SplashScreen } from 'expo-splash-screen';
+import { View, Text } from 'react-native';
 
 const Stack = createStackNavigator();
 
 export default function App() {
+  useEffect(() => {
+    // Impede que a SplashScreen se esconda automaticamente
+    SplashScreen.preventAutoHideAsync();
+
+    // Simulação de carregamento dos recursos
+    const loadResources = async () => {
+      // Carregue seus recursos, fontes, imagens, etc.
+
+      // Quando os recursos estiverem prontos, esconda a SplashScreen
+      SplashScreen.hideAsync();
+    };
+
+    loadResources();
+  }, []);
   return (
     <NavigationContainer>
       <Stack.Navigator>
